7b981ea9f23bd6aa5b6af9c69180e73e1202ce37
sajalmittal1781 <sajalmittal1781@gmail.com>
2023-12-08 18:18:25 +0530

    url

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -68,7 +68,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/signup', {
+      const response = await fetch('https://login-signup-mysql-h790wqu99-sajals-projects-c75b5687.vercel.app/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
