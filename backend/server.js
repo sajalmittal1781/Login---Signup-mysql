be67ea19a49d4ea9d109268ae482cea5a7a227cb
sajalmittal1781 <sajalmittal1781@gmail.com>
2023-12-08 18:21:35 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const cors=require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://losting.vercel.app'];
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
 // Middleware
 app.use(bodyParser.json());
